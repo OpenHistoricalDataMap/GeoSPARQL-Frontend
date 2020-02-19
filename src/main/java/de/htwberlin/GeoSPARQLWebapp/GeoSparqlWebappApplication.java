6bed1c8854425342423ef8032ac15a6b191c9e29
Rankhole <muhammed_ali96@web.de>
2020-02-19 17:34:12 +0100

    Added Spring Boot for Web App support

---
+++ src/main/java/de/htwberlin/GeoSPARQLWebapp/GeoSparqlWebappApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.GeoSPARQLWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSparqlWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSparqlWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/GeoSPARQLWebapp/GeoSparqlWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.GeoSPARQLWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSparqlWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
